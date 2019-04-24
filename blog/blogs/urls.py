cb793d230561535248b5713357fa068f634230b1
sjy <929497487@qq.com>
2019-04-24 11:47:34 +0800

    博客页面标签 分类 日期归档的跳转

--- blog/blogs/urls.py
+++ blog/blogs/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     url(r'index/$', views.index),
     url(r'single/(\d+)/$', views.single),
-    url(r'comment/(\d+)/$', views.comment)
+    url(r'comment/(\d+)/$', views.comment),
+    url(r'index/kind/(\d+)/$', views.kinds),
+    url(r'index/tally/(\d+)/$', views.tallys),
+    url(r'index/date/(.*?)/(.*?)/$', views.date),
 ]
--- blog/blogs/views.py
+++ blog/blogs/views.py
@@ -2,31 +2,31 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ARTICLE, TALLY, COMMENT, KIND
 from django.http import HttpResponseRedirect
+import datetime
 # Create your views here.
 
 
 def index(request):
-    articles = ARTICLE.objects.all()
+    articles = ARTICLE.objects.all().order_by("-date")
     tally = TALLY.objects.all()
     kind = KIND.objects.all()
-    article_new_list = []
-    for article in articles:
-        if len(article_new_list) < 2:
-            article_new_list.append(article)
-        else:
-            for a in article_new_list:
-                if article.date > a.date:
-                    article_new_list.remove(a)
-                    article_new_list.append(article)
+    article_new_list = [articles[0], articles[1], articles[2]]
+    date = datetime.date.today()
     return render(request, 'blog/index.html', {"articles": articles, "article_new_list": article_new_list,
-                                               "tally": tally, "kind": kind})
+                                               "tally": tally, "kind": kind, "date": date})
 
 
 def single(request, i):
     article = ARTICLE.objects.get(pk=i)
+    article.read_count += 1
+    article.save()
     comments = article.comment_set.all()
-    print(comments)
-    return render(request, "blog/single.html", {"i": i, "article": article, "comments": comments})
+    articles = ARTICLE.objects.all().order_by("-date")
+    tally = TALLY.objects.all()
+    kind = KIND.objects.all()
+    article_new_list = [articles[0], articles[1], articles[2]]
+    return render(request, "blog/single.html", {"i": i, "article": article, "comments": comments, "articles": articles, "article_new_list": article_new_list,
+                                               "tally": tally, "kind": kind})
 
 
 def comment(request, i):
@@ -38,3 +38,39 @@ def comment(request, i):
     c.c_content = request.POST['comment']
     c.save()
     return HttpResponseRedirect("/blog/single/%s/" % i, {"i": i, "article": article, "comments": comments})
+
+
+def kinds(request, i):
+    articles = ARTICLE.objects.all().order_by("-date")
+    tally = TALLY.objects.all()
+    kind = KIND.objects.all()
+    article_new_list = [articles[0], articles[1], articles[2]]
+    kinds = KIND.objects.get(pk=i)
+    articles = kinds.article_set.all()
+    return render(request, 'blog/index.html', {"articles": articles, "article_new_list": article_new_list,
+                                               "tally": tally, "kind": kind})
+
+
+def tallys(request, i):
+    articles = ARTICLE.objects.all().order_by("-date")
+    tally = TALLY.objects.all()
+    kind = KIND.objects.all()
+    article_new_list = [articles[0], articles[1], articles[2]]
+    tallys = TALLY.objects.get(pk=i)
+    articles = tallys.article_set.all()
+    return render(request, 'blog/index.html', {"articles": articles, "article_new_list": article_new_list,
+                                               "tally": tally, "kind": kind})
+
+
+def date(request, y, m):
+    articles = ARTICLE.objects.all().filter(date__year=y).filter(date__month=m)
+    tally = TALLY.objects.all()
+    kind = KIND.objects.all()
+    if len(articles) > 2:
+        article_new_list = [articles[0], articles[1], articles[2]]
+    else:
+        article_new_list = []
+    date = datetime.date.today()
+    return render(request, 'blog/index.html', {"articles": articles, "article_new_list": article_new_list,
+                                               "tally": tally, "kind": kind, "date": date})
+    # return HttpResponse("成功")
